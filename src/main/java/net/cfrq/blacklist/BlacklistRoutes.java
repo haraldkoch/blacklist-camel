a189eecef0c7fdfd1b92d87b69482105b6fde3bf
Harald Koch <chk@monique.chk.cfrq.net>
2018-03-15 22:56:15 -0400

    fix date formats on GET; implement CREATE

--- src/main/java/net/cfrq/blacklist/BlacklistRoutes.java
+++ src/main/java/net/cfrq/blacklist/BlacklistRoutes.java
@@ -1,6 +1,12 @@
 package net.cfrq.blacklist;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Map;
 
 import static org.apache.camel.model.rest.RestParamType.*;
 
@@ -9,6 +15,7 @@ public class BlacklistRoutes extends RouteBuilder {
 
     @SuppressWarnings("RedundantThrows")
     public void configure() throws Exception {
+
         // a few test routes just to make sure that our infrastructure is working
         rest("/say")
                 .get("/hello").id("get-hello").to("direct:hello")
@@ -38,12 +45,30 @@ public class BlacklistRoutes extends RouteBuilder {
                 .route()
                 .convertBodyTo(String.class) // this prevents an annoying "java.io.IOException: Stream closed" error
                 .to("sql:SELECT * FROM blacklist WHERE id=:#id")
+
+                // f'ing java.sql.Timestamp
+                .process(new Processor(){
+                    @SuppressWarnings("unchecked")
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        List<Map<String,Object>> results = (List<Map<String, Object>>) exchange.getIn().getBody();
+                        for (Map<String, Object> result : results) {
+                            Object date = result.get("date");
+                            // YYYY-MM-DD HH:MM:SS[.fraction]
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+                            result.put("date", sdf.format(date));
+                        }
+
+                        exchange.getIn().setBody(results);
+                    }
+                })
                 .endRest()
 
             .post("/").id("create-entry").description("create a new blacklist entry")
                 .param().name("body").type(body).description("The entry to create").endParam()
                 .route()
-                .transform().simple("creating new entry ${body}")
+                .to("sql:INSERT INTO blacklist (ip, type, date, description) VALUES (:#ip, :#type, :#date, :#description)")
+                .transform().simple("inserted ${body} rows.")
                 .endRest()
 
             .put("/{id}").id("update-entry").description("update existing entry")
